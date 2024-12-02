c6665a427fa596f14d1060270aeec72cb3663d39
Joscha Germerott <joscha.germerott@googlemail.com>
2024-12-02 22:47:34 +0100

    Adapt naming scheme for Chapter Officers

---
+++ Quartermaster.Data/ChapterAssociates/ChapterOfficer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Quartermaster.Data.ChapterAssociates;
+
+public class ChapterOfficer {
+    public Guid UserId { get; set; }
+    public Guid ChapterId { get; set; }
+    public ChapterOfficerType AssociateType { get; set; }
+}
+
+public enum ChapterOfficerType {
+    /// <summary>Chair</summary>
+    Captain,
+    /// <summary>Vice-Chair</summary>
+    FirstOfficer,
+    Quartermaster,
+    Treasurer,
+    ViceTreasurer,
+    PoliticalDirector,
+    /// <summary>Officer without further specification</summary>
+    Member
+}
